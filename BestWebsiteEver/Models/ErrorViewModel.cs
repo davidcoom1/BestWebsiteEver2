b90ffec3668d71040a863a0c8073ee78e533a703
fivenightsdave <75261394+davidcoom1@users.noreply.github.com>
2024-05-09 12:48:37 +0100

    Add project files.

---
+++ BestWebsiteEver/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BestWebsiteEver.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
